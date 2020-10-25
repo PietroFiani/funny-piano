64ee6f9cae1e2a55c483c35b7dca28a0ed2de579
PietroFiani <fiani.pietro@gmail.com>
2020-10-25 21:20:39 +0100

    update 089869

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,18 @@ export default {
 </script>
 
 <style lang="postcss">
-.body {
-  height:100vh; 
-  background:linear-gradient(135deg, #FF9D6C 0%, #BB4E75 100%);
+.body{
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(135deg, #FF9D6C 0%, #BB4E75 100%); 
 }
 .app-container{
   width: 100%;
   height: 100%;
-  
 }
-
+#app{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
 </style>
